eb7a8fe638e47f40c666eeb337ff80f792a2592a
yafengxiang <yafengxiang@163.com>
2013-12-15 16:59:40 +0800

    提交测试

--- linq001/Program.cs
+++ linq001/Program.cs
@@ -10,6 +10,36 @@ namespace linq001
     {
         static void Main(string[] args)
         {
+
+            // Specify the data source.
+            int[] scores = new int[] { 97, 92, 81, 60 };
+
+            // Define the query expression.
+            IEnumerable<int> scoreQuery =
+                from score in scores
+                where score > 80
+                orderby score descending
+                select score;
+
+            var scoreQ = from score in scores
+                         where score > 80
+                         select score;
+
+            IEnumerable<string> highScores = from score in scores
+                                             where score > 80
+                                             orderby score ascending
+                                             select string.Format("the high score is{0}", score);
+
+            foreach (int i in scoreQ)
+            {
+                Console.WriteLine(i + ",");
+            }
+            foreach (string s in highScores)
+            {
+                Console.WriteLine(  s);
+            }
+
+            Console.ReadKey();
         }
     }
 }
